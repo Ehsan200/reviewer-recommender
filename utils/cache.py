0c9925c9de2d6806d0352d96893c2a8d4d8fd4db
Ehsan200 <ehsan.movaffagh@icloud.com>
2024-01-16 18:29:22 +0330

    feat: Add cache utility

---
+++ utils/cache.py
@@ -0,0 +1,28 @@
+import pickle
+import os
+
+CACHE_DIR = '.cache'
+if not os.path.exists(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
+
+
+class Cache:
+
+    @staticmethod
+    def _get_file_location(key):
+        return f'{CACHE_DIR}/{key}'
+
+    @classmethod
+    def load(cls, key):
+        final_filepath = cls._get_file_location(key)
+        if not os.path.exists(final_filepath):
+            return None
+
+        with open(final_filepath, 'rb') as f:
+            return pickle.load(f)
+
+    @classmethod
+    def store(cls, key, data):
+        final_filepath = cls._get_file_location(key)
+        with open(final_filepath, 'wb') as f:
+            pickle.dump(data, f)
