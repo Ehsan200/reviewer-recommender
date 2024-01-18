2e8d16599a5ec94d723dbd1aee5e4279d9d9e9f5
Ehsan200 <ehsan.movaffagh@icloud.com>
2024-01-18 17:10:37 +0330

    fix: Fix cache system of file similarities

--- algorithms/utils/rev_finder/utility.py
+++ algorithms/utils/rev_finder/utility.py
@@ -23,9 +23,21 @@ class ProjectFilesSimilarity:
     def _cache_filepath(self):
         return f'{self._manager.project}.files-similarities'
 
+    def _get_cache_filepath(self, methodology):
+        return f'{self._cache_filepath}-{methodology.__name__}'
+
     @cached_property
     def _cached_scores(self):
-        return Cache.load(self._cache_filepath)
+        data = {}
+        for methodology in METHODOLOGIES:
+            cached_data = Cache.load(
+                self._get_cache_filepath(methodology),
+            )
+            if not cached_data:
+                return None
+            data[methodology.__name__] = cached_data
+
+        return data
 
     def calculate_scores(self):
         if self._from_cache and self._cached_scores:
@@ -47,9 +59,28 @@ class ProjectFilesSimilarity:
                 score = self._get_file_path_similarity(f1, f2, methodology)
                 self._scores[methodology.__name__][(f1, f2)] = score
                 self._scores[methodology.__name__][(f2, f1)] = score
+        for f1 in self._manager.files_list:
+            for methodology in METHODOLOGIES:
+                self._scores[methodology.__name__][(f1.filepath, f1.filepath)] = self._get_file_path_similarity(
+                    f1.filepath,
+                    f1.filepath,
+                    methodology,
+                )
 
         info_logger.info('File similarity scores calculated')
-        Cache.store(self._cache_filepath, self._scores, chunk=True)
+        self._store()
+
+    def _store(self):
+        for methodology in METHODOLOGIES:
+            Cache.store(
+                self._get_cache_filepath(methodology),
+                self._scores[methodology.__name__],
+                chunk=True,
+            )
 
     def get_file_similarity(self, f1, f2, methodology):
+        if (f1, f2) not in self._scores[methodology.__name__]:
+            score = self._get_file_path_similarity(f1, f2, methodology)
+            self._scores[methodology.__name__][(f1, f2)] = score
+            self._scores[methodology.__name__][(f2, f1)] = score
         return self._scores[methodology.__name__][(f1, f2)]
